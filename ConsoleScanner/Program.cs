4e64e9ec66233a6f5dd7780be317350f7b2b2cbd
Chris Zielin <chris@zielin.com>
2020-05-30 15:09:52 -0700

    Add initial console scan project. #35

---
+++ ConsoleScanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
