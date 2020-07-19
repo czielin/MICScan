0cf75bfc1e44650ea5e7a59baac848c6b25971e0
Chris Zielin <chris@zielin.com>
2020-07-18 19:23:59 -0700

    - Add top level exception handling. - Add check for null ArgumentList for object creation expressions. AB#118 - Add command line scan screenshot.

--- Scan/Program.cs
+++ Scan/Program.cs
@@ -14,47 +14,55 @@ namespace Scan
         static async Task<int> Main(string[] args)
         {
             int exitCode = -1;
-            Scanner scanner = new Scanner(true);
-            if (args.Length != 2)
+
+            try
             {
-                Console.Error.WriteLine("Missing expected arguments: Scan.exe [Project File Name] [Source Path]");
-            }
-            else
-            {
-                string projectFileName = args[0];
-                string sourcePath = args[1];
-                string[] projectFilePaths = Directory.GetFiles(sourcePath, projectFileName, SearchOption.AllDirectories);
-                if (projectFilePaths.Length == 0)
+                Scanner scanner = new Scanner(true);
+                if (args.Length != 2)
                 {
-                    Console.Error.WriteLine("The project file was not found.");
-                }
-                else if (projectFilePaths.Length > 1)
-                {
-                    Console.Error.WriteLine("There was more than one project file with this name found.");
+                    Console.Error.WriteLine("Missing expected arguments: Scan.exe [Project File Name] [Source Path]");
                 }
                 else
                 {
-                    //var csFiles = await scanner.ScanFolder(@"C:\git\sard\TestApplication");
-                    List<Example> classifiedFiles = await scanner.ScanProject(projectFilePaths[0]);
-                    ConsoleColor originalColor = Console.ForegroundColor;
-                    string flawDescription;
-                    foreach (Example file in classifiedFiles)
+                    string projectFileName = args[0];
+                    string sourcePath = args[1];
+                    string[] projectFilePaths = Directory.GetFiles(sourcePath, projectFileName, SearchOption.AllDirectories);
+                    if (projectFilePaths.Length == 0)
                     {
-                        if (file.ClassName == "No Flaw")
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            flawDescription = "";
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            flawDescription = $"{Environment.NewLine}\t{GetLabelDescription(file.ClassName)}";
-                        }
-                        Console.WriteLine($"[{file.ClassName}] {file.SourcePath} {flawDescription}");
+                        Console.Error.WriteLine("The project file was not found.");
                     }
-                    Console.ForegroundColor = originalColor;
-                    exitCode = classifiedFiles.Count(f => f.ClassName != "No Flaw");
+                    else if (projectFilePaths.Length > 1)
+                    {
+                        Console.Error.WriteLine("There was more than one project file with this name found.");
+                    }
+                    else
+                    {
+                        //var csFiles = await scanner.ScanFolder(@"C:\git\sard\TestApplication");
+                        List<Example> classifiedFiles = await scanner.ScanProject(projectFilePaths[0]);
+                        ConsoleColor originalColor = Console.ForegroundColor;
+                        string flawDescription;
+                        foreach (Example file in classifiedFiles)
+                        {
+                            if (file.ClassName == "No Flaw")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                flawDescription = "";
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                flawDescription = $"{Environment.NewLine}\t{GetLabelDescription(file.ClassName)}";
+                            }
+                            Console.WriteLine($"[{file.ClassName}] {file.SourcePath} {flawDescription}");
+                        }
+                        Console.ForegroundColor = originalColor;
+                        exitCode = classifiedFiles.Count(f => f.ClassName != "No Flaw");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
             }
 
             return exitCode;
