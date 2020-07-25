415e7ec25b3e6fa3f232b5a4e608d0b2ecde5386
Chris Zielin <chris@zielin.com>
2020-07-25 13:32:47 -0700

    Add test web API application.

---
+++ TestUserApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TestUserApi.Models;
+
+namespace TestUserApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        public async Task<User> GetUser(string username)
+        {
+            SqlCommand sqlCommand = new SqlCommand($"SELECT * FROM Users WHERE Username = '{username}'");
+            SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+            return new User
+            {
+                FirstName = (string)reader["FirstName"],
+                LastName = (string)reader["LastName"]
+            };
+        }
+    }
+}
