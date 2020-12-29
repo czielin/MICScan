2fd83b729682d729aaec9f44547f3a1f98010660
Chris Zielin <chris@zielin.com>
2020-12-28 19:46:19 -0800

    Remove integration test files now that changes have been verified.

--- Classify/utils.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def json_equals(json_obj1, json_obj2):
-    """Test equality of json objects"""
-    return json.dumps(json_obj1, sort_keys=True) == json.dumps(json_obj2, sort_keys=True)
